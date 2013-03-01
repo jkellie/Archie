0bc9ee1880ea31aeb9c6e8caa65809784b0d9352
Jason Kellie <jkellie@me.com>
2013-03-01 14:11:57 -0500

    configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Archie::Application.routes.draw do
+  
+  devise_for :users, :path => 'admin'
+  
+  namespace :admin do
+    resources :users
+  end
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
