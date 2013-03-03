fa15ca1d5027891185f9fbc305e6ea89de51390f
Jason Kellie <jkellie@me.com>
2013-03-03 15:26:24 -0500

    generate team model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Archie::Application.routes.draw do
   
   devise_for :users, :path => 'admin', :controllers => { :registrations => "registrations"}
-
-  
+  resources :teams
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
