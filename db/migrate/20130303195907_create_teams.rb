f30ecf5f18a9d92397a8f7897563d18f2adba5f3
Jason Kellie <jkellie@me.com>
2013-03-09 13:37:24 -0500

    rake migration

--- db/migrate/20130303195907_create_teams.rb
+++ db/migrate/20130303195907_create_teams.rb
@@ -1,8 +1,13 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
+      t.string  :name
+      t.string  :logo
+      t.intiger :provider_id
 
       t.timestamps
     end
+    
+    add_index :teams, :name, :unique => true
   end
 end
