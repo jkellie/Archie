9c60de04bd828ca1031e30913bde92281820fa7a
Jason Kellie <jkellie@me.com>
2013-03-12 14:14:28 -0400

    add current user and provider methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  
+  def current_team
+    
+  end
+  
+  def current_provider
+    if current_user
+      @current_provider = current_user.provider
+    else
+      Provider.find_by_subdomain! request.subdomain
+    end
+  end
+  helper_method :current_provider
+    
 end
